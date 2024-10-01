603d83f93e337aa6abc9d791f1fa23be88d4d2e4
Can335 <canlahu1@gmail.com>
2024-10-01 19:51:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("YOUR GAY");
+
+let person = {
+
+    name : "Cosmin",
+    Airstrike : true
+
+};
+
+function Greet(Person) {
+
+alert(Person.name + " " + " Airstrike?" + Person.Airstrike)
+
+};
+
+Greet(person)
